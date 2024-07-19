2c890bfda6c094680112418679e478114fde7801
Kevin-gram <k.nyiringan@alustudent.com>
2024-07-19 11:55:30 +0200

    installing react and tailwind

---
+++ Business card/src/App.jsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <div>
+      <img
+        src="./assets/e7a124a5078b73381f7522b0a45212cd.jpg"
+        alt=""
+        className="w-[317px] h-[317]"
+      />
+    </div>
+  );
+}
